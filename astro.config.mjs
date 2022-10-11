f2af715fa477dbf4291d2929638abbb23174bbf8
David DeWert <daviddewert@gmail.com>
2022-10-10 22:03:26 -0700

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image()],
+  build: {
+    trailingSlash: 'never',
+    format: 'directory'
+  }
+});
